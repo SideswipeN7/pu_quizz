68a9f1e2e96b3801cbb3ebe7f4d70d0222e8bde0
Paweł Wanot <pawel.wanot@gmail.com>
2018-10-09 21:21:18 +0200

    New controller and updated gitignore

---
+++ implementation/QuizzApp/QuizzApp/Controllers/TopTenController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace QuizzApp.Controllers
+{
+    public class TopTenController : Controller
+    {
+        // GET: TopTen
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
