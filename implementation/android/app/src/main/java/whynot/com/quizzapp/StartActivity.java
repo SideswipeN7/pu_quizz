06393f8a6185f73e998e03b290339625f830ee71
slonskiziemniak <pu@cebula.j.pl>
2018-10-16 23:09:02 +0200

    added_newbuttons

--- implementation/android/app/src/main/java/whynot/com/quizzapp/StartActivity.java
+++ implementation/android/app/src/main/java/whynot/com/quizzapp/StartActivity.java
@@ -90,7 +90,7 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
 
         mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
+     //   mControlsView = findViewById(R.id.fullscreen_content_controls);
         mContentView = findViewById(R.id.fullscreen_content);
 
 
@@ -105,7 +105,7 @@ public class StartActivity extends AppCompatActivity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+
     }
 
     @Override
